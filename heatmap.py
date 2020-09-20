e2572a21b9bbdbb0f36bbb1c0404d0b97b1395fc
Chirag Vartak <vartak.chirag.8@gmail.com>
2020-09-20 17:55:38 +0530

    Complete task 3

--- heatmap.py
+++ heatmap.py
@@ -1,44 +1,30 @@
-import utils
-from sensor_series import SensorSeries
-from vector_set import VectorSet
-import numpy as np
-from typing import Tuple, List
-from word import Word
-import seaborn as sns
+from typing import List
+
 import matplotlib.pyplot as plt
+import seaborn as sns
+from pandas import DataFrame
 
-class Heatmap:
-    ndarray: np.ndarray
-    _number_of_sensors: int
-    _number_of_words: int
-    _shape: Tuple[int,int]
 
-    def __init__(self, vector_set:VectorSet, possible_words_list:List[Word]):
-        self._number_of_sensors = len(vector_set)
-        self._number_of_words = len(possible_words_list)
-        self._shape = (self._number_of_sensors, self._number_of_words)
-        M = []
-        for sensor_id in sorted(vector_set.vector_set.keys()):
-            sensor_vector = vector_set[sensor_id]
-            L = []
-            for word in possible_words_list:
-                L.append(sensor_vector[word] if (word in sensor_vector) else 0.0)
-            M.append(tuple(L))
-        self.ndarray = np.array(M)
+class HeatMap:
+    df: DataFrame
+    xlabels: List
 
-    def plot_heatmap(self):
-        sns.heatmap(self.ndarray, square=True)
-        plt.savefig("heatmap.png")
-        plt.show()
+    def __init__(self, df: DataFrame, xlabels: List):
+        if df.shape[1] != len(xlabels):
+            raise Exception("Length of the xlabels list should match the number of dataframe columns")
+        self.df = df
+        self.xlabels = xlabels
 
-    def plot_gray_heatmap(self):
-        cmap = sns.cubehelix_palette(50, hue=0.05, rot=0, light=0.9, dark=0, as_cmap=True)
-        sns.heatmap(self.ndarray, cmap=cmap, square=True, cbar=False)
-        plt.savefig("heatmap.png")
-        plt.show()
-
-    def __str__(self):
-        return str(self.ndarray)
-
-    def __repr__(self):
-        return str(self.ndarray)
+    def plot(self, output_file_path, title):
+        df = self.df.loc[:, (self.df != 0).any(axis=0)]
+        plt.figure(figsize=(10, 7))
+        hm = sns.heatmap(
+            df, cmap="gray_r", square=True,
+            linewidths=0.1)
+        for _, spine in hm.spines.items():
+            spine.set_visible(True)
+        hm.set_yticklabels(hm.get_yticklabels(), rotation=0)
+        hm.set_xticklabels(hm.get_xticklabels(), rotation=90)
+        hm.set_title(title)
+        plt.savefig(output_file_path)
+        # plt.show()
--- task3.py
+++ task3.py
@@ -1,16 +1,38 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
+import sys
+from os.path import abspath
+from os.path import join
+
+from pandas import DataFrame
+
+import utils
 from database import Database
-import constants
-from word import Word
-from pprint import pprint
+from heatmap import HeatMap
 from vector_set import VectorSet
 
+
+def parse_inputs(arguments):
+    if len(arguments) != 4:
+        raise Exception("Run the program as: python3 task3.py dir=<wrd-files-dir> f=1.csv type=tf")
+    wrd_files_directory = abspath(arguments[1][4:])
+    gesture_name = arguments[2][2:].split('.')[0]
+    value_type = arguments[3][5:]
+    return (wrd_files_directory, gesture_name, value_type)
+
+
 if __name__ == "__main__":
-    database = Database(constants.RESULTS_DIRECTORY_PATH, filenames_ending_with="_wordified.csv")
-    database.applymap(Word.parse)
-    gesture = database.get_gesture("1_wordified")
-    vector_set = VectorSet.get_vector_set_representation_of_gesture(database, "1_wordified", method="tf")
-    pprint(vector_set)
+    WRD_FILES_DIRECTORY, GESTURE_NAME, TYPE = parse_inputs(sys.argv)
+
+    db = Database.from_wrd_files(WRD_FILES_DIRECTORY)
+    vector_set = VectorSet.get_vector_set_representation_of_gesture(db, GESTURE_NAME, method=TYPE)
+    words_list = utils.words_in_database(db)
+    df = DataFrame(
+        data=vector_set.get_array(words_list),
+        columns=words_list
+    )
+    hm = HeatMap(df, words_list)
+    output_file_path = join(WRD_FILES_DIRECTORY, GESTURE_NAME + "-" + TYPE + ".png")
+    hm.plot(
+        output_file_path,
+        title="Heatmap for " + GESTURE_NAME + ".csv using " + TYPE.upper() + " values"
+    )
+    print("Heatmap generated at", output_file_path)
