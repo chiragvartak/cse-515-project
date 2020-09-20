5dc504c036219023b816783e8d8ef36e5036a4a7
Chirag Vartak <vartak.chirag.8@gmail.com>
2020-09-20 05:39:45 +0530

    Complete task 4

--- task4.py
+++ task4.py
@@ -1,15 +1,37 @@
-import numpy as np
-from database import Database
-from utils import words_in_database
-from heatmap import Heatmap
-from sensor_vector import SensorVector
-from vector_set import VectorSet
+import sys
+from os.path import abspath
 
-def euclidean_distance_between_vector_sets(database:Database, vs1:VectorSet, vs2:VectorSet) -> float:
-    occurring_words_in_database = words_in_database(database)
-    heatmap1 = Heatmap(vs1, occurring_words_in_database)
-    heatmap2 = Heatmap(vs2, occurring_words_in_database)
-    return np.linalg.norm(heatmap1.ndarray - heatmap2.ndarray)
+import numpy as np
+
+import utils
+from database import Database
+
+
+def parse_inputs(arguments):
+    if len(arguments) != 4:
+        raise Exception("Run the program as: python3 task4.py dir=<wrd-files-dir> f=1.csv type=tf")
+    wrd_files_directory = abspath(arguments[1][4:])
+    gesture_name = arguments[2][2:].split('.')[0]
+    type = arguments[3][5:]
+    return (wrd_files_directory, gesture_name, type)
+
 
 if __name__ == "__main__":
-    raise NotImplementedError
+    WRD_FILES_DIRECTORY, GESTURE_NAME, TYPE = parse_inputs(sys.argv)
+
+    # Initialize all the gesture vectors
+    db = Database.from_wrd_files(WRD_FILES_DIRECTORY)
+    db.compute_ndarrays_for_gestures(method=TYPE, possible_words_list=utils.words_in_database(db))
+
+    # Compare the queried gesture with all other gestures in the database
+    query_gesture = db.get_gesture(GESTURE_NAME)
+    distances = []
+    for gesture in db:
+        distances.append((abs(np.linalg.norm(query_gesture.ndarray - gesture.ndarray)), gesture.gesture_name))
+    distances.sort()
+    print("The 10 most similar gestures are:")
+    i = 1
+    for distance, gesture_name in distances:
+        if i > 10: break
+        print(gesture_name + ".csv with distance", round(distance, 4))
+        i += 1
