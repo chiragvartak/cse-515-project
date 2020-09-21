99f5f9840b12184a48bd0801d88f6ad7b00d8b1c
Chirag Vartak <vartak.chirag.8@gmail.com>
2020-09-21 07:03:22 +0530

    Remove unnecessary constants

--- constants.py
+++ constants.py
@@ -1,16 +1,6 @@
-RESULTS_DIRECTORY_PATH = 'D:\\Google Drive\\ASU\\CSE 515 Multimedia and Web Databases\\Project\\results'
-WORDIFIED_FILE_PATH = 'D:\\Google Drive\\ASU\\CSE 515 Multimedia and Web Databases\\Project\\results\\1_wordified.csv'
-
-#
-GESTURES_DIRECTORY_PATH = 'D:\\Google Drive\\ASU\\CSE 515 Multimedia and Web Databases\\Project'
-OUTPUT_DIRECTORY="D:\\Google Drive\\ASU\\CSE 515 Multimedia and Web Databases\\Project\\output"
-EXTRA_DIRECTORY="D:\\Google Drive\\ASU\\CSE 515 Multimedia and Web Databases\\Project\\extra"
 NORMALIZED_GESTURE_FILE_SUFFIX = "_normalized.csv"
 QUANTIZED_GESTURE_FILE_SUFFIX = "_quantized.csv"
 WORDIFIED_GESTURE_FILE_SUFFIX = "_wordified.csv"
 WRD_FILE_SUFFIX = ".wrd"
-RESOLUTION = 3
 MU = 0.0
 SIGMA = 0.25
-WINDOW_LENGTH = 3
-SHIFT_LENGTH = 1
